7b5bdbc97965304f47e6394723b1658f0b66a6e5
sooom1627 <swfh2367@gmail.con>
2021-04-25 14:54:36 +0900

    minimum change

--- revs/settings.py
+++ revs/settings.py
@@ -78,21 +78,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'revs.wsgi.application'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
