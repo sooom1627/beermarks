2070d6c1174a361237e3b017ce7f17cbf924e3b3
sooom1627 <swfh2367@gmail.con>
2021-04-28 17:16:20 +0900

    url refactoring

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Cre, Cre_b, Det, productView, BrandView, searchproduct, BrandDetail, detail
+from .views import  productView, BrandView, searchproduct, BrandDetail, detail
+
 urlpatterns = [
-    #path("", List.as_view(), name="list"),
-    path("create/", Cre.as_view(), name="create"),
-    path("create_b/", Cre_b.as_view(), name="create_b"),
     path("detail/<int:pk>", detail, name="detail"),
-    #path("detail2/<int:pk>", detail, name="detail2"),
     path("list/", productView, name="list"),
     path("brand/", BrandView.as_view(), name="brandlist"),
     path("brand/detail/<int:pk>", BrandDetail.as_view(), name="branddetail"),
--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path("logout/", logoutFunc, name="logout"),
     path('mypage/<int:pk>/', mypage, name="mypage"),
     path('userlist/', UserList.as_view(), name="userlist"),
-    path('udetexe/<int:pk>/', UserDetail.as_view(), name='udet2'),
     path("udet/<int:pk>/", userdetail, name="udet"),
 ]
