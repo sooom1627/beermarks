b87dd0c0b9498d6326f6f948099fca65d6d48e5a
sooom1627 <swfh2367@gmail.con>
2021-05-02 16:28:14 +0900

    nav_cust edit

--- static/js/nav_cust.js
+++ static/js/nav_cust.js
@@ -9,3 +9,45 @@ $(document).ready(function () {
     $("#sidebar").removeClass("iq-sidebar-active");
   });
 });
+
+/*---------------------------------------------------------------------
+        Search input
+        -----------------------------------------------------------------------*/
+jQuery(document).on("click", function (e) {
+  let myTargetElement = e.target;
+  let selector, mainElement;
+  if (
+    jQuery(myTargetElement).hasClass("search-toggle") ||
+    jQuery(myTargetElement).parent().hasClass("search-toggle") ||
+    jQuery(myTargetElement).parent().parent().hasClass("search-toggle")
+  ) {
+    if (jQuery(myTargetElement).hasClass("search-toggle")) {
+      selector = jQuery(myTargetElement).parent();
+      mainElement = jQuery(myTargetElement);
+    } else if (jQuery(myTargetElement).parent().hasClass("search-toggle")) {
+      selector = jQuery(myTargetElement).parent().parent();
+      mainElement = jQuery(myTargetElement).parent();
+    } else if (
+      jQuery(myTargetElement).parent().parent().hasClass("search-toggle")
+    ) {
+      selector = jQuery(myTargetElement).parent().parent().parent();
+      mainElement = jQuery(myTargetElement).parent().parent();
+    }
+    if (
+      !mainElement.hasClass("active") &&
+      jQuery(".navbar-list li").find(".active")
+    ) {
+      jQuery(".navbar-list li").removeClass("iq-show");
+      jQuery(".navbar-list li .search-toggle").removeClass("active");
+    }
+
+    selector.toggleClass("iq-show");
+    mainElement.toggleClass("active");
+
+    e.preventDefault();
+  } else if (jQuery(myTargetElement).is(".search-input")) {
+  } else {
+    jQuery(".navbar-list li").removeClass("iq-show");
+    jQuery(".navbar-list li .search-toggle").removeClass("active");
+  }
+});
