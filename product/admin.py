0fa511554037a575874f1f76bc81a1c7177ec228
sooom1627 <swfh2367@gmail.con>
2021-05-28 18:52:13 +0900

    brand model edit

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Brand, Product, Type
+from .models import Brand, Product, Type, Medium, Locate
 
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(Product)
 admin.site.register(Type)
+admin.site.register(Locate)
+admin.site.register(Medium)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.db.models.fields import TextField
 # Create your models here.
+
+class Locate(models.Model):
+    country = models.CharField(max_length=50, default="none", null=True)
+    state = models.CharField(max_length=100, default="none", null=True)
 
 class Brand(models.Model):
     bname = models.CharField(max_length=50)
@@ -7,9 +12,21 @@ class Brand(models.Model):
     bpic = models.ImageField(upload_to='images/brand/', height_field=None, width_field=None, max_length=None)
     bname_search_index = models.TextField(blank=True)
     locate = models.CharField(max_length=50, null=True, blank=True)
-    
+    country = models.ForeignKey(Locate, on_delete=models.CASCADE, related_name="brand_locate", null=True)
+
     def __str__(self):
         return self.bname
+
+class Medium(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    website = models.TextField(null=True)
+    ecsite = models.TextField( null=True )
+    facebook = models.TextField(null=True)
+    instagram = models.TextField(null=True)
+    twitter = models.TextField(null=True)
+
+    def __str__(self):
+        return self.brand
 
 class Type(models.Model):
     ptype = models.CharField(max_length=50)
