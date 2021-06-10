f634c5809f8c66c20c11fed9c25537e4a2bb8073
sooom1627 <swfh2367@gmail.con>
2021-06-10 23:22:22 +0900

    debug False

--- revs/settings.py
+++ revs/settings.py
@@ -108,7 +108,7 @@ if "SoomMacBook-Air" in hostname:
     )
 else:
     # 本番環境
-    DEBUG = True
+    DEBUG = False
     import dj_database_url
     db_from_env = dj_database_url.config()
     DATABASES = {
